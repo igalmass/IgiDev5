853c8c167d642a3fa7bcebff52df456e9ed53eda
igalm <igalmass@hotmail.com>
2018-04-25 14:20:11 +0300

    Basic udmy - local ref tutorial

---
+++ UdemyAngTut/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,39 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  //newServerName = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    //console.log(this.serverContentInput);
+    // console.log(nameInput.value);
+    this.serverCreated.emit(
+      {
+        serverName: nameInput.value,
+        serverContent: this.serverContentInput.nativeElement.value});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+
+  }
+
+}
