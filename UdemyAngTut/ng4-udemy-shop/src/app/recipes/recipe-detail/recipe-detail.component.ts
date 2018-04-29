afe69f76b8ae826b7f348aa4c451b0314ef83fa1
igalm <igalmass@hotmail.com>
2018-04-29 14:04:03 +0300

    adding routing to course project

--- UdemyAngTut/ng4-udemy-shop/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ UdemyAngTut/ng4-udemy-shop/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,15 +10,24 @@ import {RecipeService} from "../recipe.service";
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe : Recipe;
+  recipe : Recipe;
+  id: number;
 
-  constructor(private recipeService :RecipeService) { }
+  constructor(private recipeService :RecipeService, private route: ActivatedRoute, private router: Router) { console.log('from ctor'); }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      });
   }
 
   onAddToShoppingList() {
-    debugger;
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
