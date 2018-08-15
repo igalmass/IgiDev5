65359509d6072a9b980a412bcdb0f29a7d3bf10a
igal.messika@mavenir.com <igal.messika@mavenir.com>
2018-08-15 14:47:04 -0400

    add navbar

--- BookLoc_Ng_Node/ng-mat-client/src/app/app.module.ts
+++ BookLoc_Ng_Node/ng-mat-client/src/app/app.module.ts
@@ -3,22 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
 import {MatTableModule} from '@angular/material/table';
 import { BookListComponent } from './components/books/book-list/book-list.component';
 import {BookService} from "./services/book.service";
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatTableModule
+    MatTableModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [ BookService ],
   bootstrap: [ AppComponent ]
